1a392f43d9baf3fe08880a540cff76c39cdfe3c7
Parinay Patel <76627704+Parinay-Patel@users.noreply.github.com>
2022-08-20 10:35:42 +0530

    Create Count_number_of_vowel_in_String.py

---
+++ Count_number_of_vowel_in_String.py
@@ -0,0 +1,26 @@
+# Python Program to Count the Number of Each Vowel
+
+string = (input("Enter a stirng : "))
+count = 0
+if 'a' in string : 
+    count = count+1
+if 'e' in string : 
+    count = count+1
+if 'i' in string : 
+    count = count+1
+if 'o' in string : 
+    count = count+1
+if 'u' in string : 
+    count = count+1
+if 'A' in string : 
+    count = count+1
+if 'E' in string : 
+    count = count+1
+if 'I' in string : 
+    count = count+1
+if 'O' in string : 
+    count = count+1
+if 'U' in string : 
+    count = count+1
+
+print(f"Number of vowel in string : {count}")
