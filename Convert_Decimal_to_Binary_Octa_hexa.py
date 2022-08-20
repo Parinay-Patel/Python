dd04ff6b493424a28b8976ac745ed28c5bb9d271
Parinay Patel <76627704+Parinay-Patel@users.noreply.github.com>
2022-08-20 10:30:02 +0530

    Create Convert_Decimal_to_Binary_Octa_hexa.py

---
+++ Convert_Decimal_to_Binary_Octa_hexa.py
@@ -0,0 +1,8 @@
+# Python Program to Convert Decimal to Binary, Octal and Hexadecimal
+
+n = int(input("Enter a decimal number : "))
+
+print(f"The decimal value of {n} is:")
+print(f"{bin(n)} in binary")
+print(f"{oct(n)} in octal.")
+print(f"{hex(n)} in hexadecimal.")
